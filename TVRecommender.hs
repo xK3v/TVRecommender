14d072ec28f02599fb7e70d0abd345a04457e6ac
xK3v <kazianschuetz.kevin@gmail.com>
2018-05-16 23:02:34 +0200

    Feature Set 0 - The Control Loop

--- TVRecommender.hs
+++ TVRecommender.hs
@@ -1,14 +1,24 @@
 --Written by Kevin Kazianschütz
 
-main :: IO ()
+main :: IO () --Einstiegspunkt
 main = do
   putStrLn ""
   putStrLn "Loading TVRecommender..."
   putStrLn ""
   printHelp
+  mainmenu
 
 
-printHelp :: IO ()
+mainmenu :: IO () --Nimmt Eingabe entgegen und leitet entsprechend weiter
+mainmenu = do
+  input <- getLine
+  case input of
+    "help" -> printHelp >> mainmenu
+    "exit" -> putStrLn "Thanks for using TVRecommender!"
+    _ -> putStrLn ("Command '" ++ input ++ "' is unknown!\n\nPlease enter a command or type 'help' for assistance!") >> mainmenu
+
+
+printHelp :: IO () --Gibt Liste der möglichen Befehle sowie Aufforderung zur Eingabe aus
 printHelp = do
   putStrLn "This Program supports the following commands:"
   putStrLn "\t 'help' ... shows this message"
