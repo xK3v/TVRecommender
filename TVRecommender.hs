8c30ffb6d3de28dd7c8ac08f618b021877be3cf2
xK3v <kazianschuetz.kevin@gmail.com>
2018-05-19 01:31:00 +0200

    more new lines consistency

--- TVRecommender.hs
+++ TVRecommender.hs
@@ -14,7 +14,7 @@ mainmenu = do
   case input of
     "help" -> printHelp >> mainmenu
     "exit" -> putStrLn "Thanks for using TVRecommender!"
-    _ -> putStrLn ("Command '" ++ input ++ "' is unknown!\n\nPlease enter a command or type 'help' for assistance!") >> mainmenu
+    _ -> putStrLn ("Command '" ++ input ++ "' is unknown!") >> mainmenu
 
 
 printHelp :: IO () --Gibt Liste der möglichen Befehle sowie Aufforderung zur Eingabe aus
