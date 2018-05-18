879c08a4c58161608c15acc090af79da51336f8e
xK3v <kazianschuetz.kevin@gmail.com>
2018-05-19 01:19:50 +0200

    list actors added

--- TVRecommender.hs
+++ TVRecommender.hs
@@ -1,18 +1,21 @@
 --Written by Kevin Kazianschütz
 
+import System.IO
+import System.Directory
+
 main :: IO () --Einstiegspunkt
 main = do
-  putStrLn ""
-  putStrLn "Loading TVRecommender..."
-  putStrLn ""
+  putStrLn "\nLoading TVRecommender..."
   printHelp
   mainmenu
 
 
 mainmenu :: IO () --Nimmt Eingabe entgegen und leitet entsprechend weiter
 mainmenu = do
+  putStrLn "\nPlease enter a command or type 'help' for assistance!"
   input <- getLine
   case input of
+    "list actors" -> getActors >> mainmenu
     "help" -> printHelp >> mainmenu
     "exit" -> putStrLn "Thanks for using TVRecommender!"
     _ -> putStrLn ("Command '" ++ input ++ "' is unknown!\n\nPlease enter a command or type 'help' for assistance!") >> mainmenu
@@ -20,8 +23,15 @@ mainmenu = do
 
 printHelp :: IO () --Gibt Liste der möglichen Befehle sowie Aufforderung zur Eingabe aus
 printHelp = do
-  putStrLn "This Program supports the following commands:"
+  putStrLn "\nThis Program supports the following commands:"
+  putStrLn "\t 'list actors' ... shows a list of all your favourite actors"
   putStrLn "\t 'help' ... shows this message"
   putStrLn "\t 'exit' ... terminate the application"
+
+getActors :: IO ()
+getActors = do
   putStrLn ""
-  putStrLn "Please enter a command or type 'help' for assistance!"
+  handle <- openFile "actors.txt" ReadMode
+  actorText <- hGetContents handle
+  putStrLn actorText
+  hClose handle
