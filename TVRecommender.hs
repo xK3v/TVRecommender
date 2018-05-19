db2b7f3dd345b638b282d20df10bfe8235b07eb3
xK3v <kazianschuetz.kevin@gmail.com>
2018-05-19 03:08:25 +0200

    first implementation of addActor

--- TVRecommender.hs
+++ TVRecommender.hs
@@ -14,7 +14,8 @@ mainMenu :: IO () --Nimmt Eingabe entgegen und leitet entsprechend weiter
 mainMenu = do
   putStrLn "\nPlease enter a command or type 'help' for assistance!"
   input <- getLine
-  case input of
+  case unwords $ take 2 $ words input of
+    "add actor" -> addActor (unwords $ drop 2 $ words input) >> mainMenu
     "list actors" -> getActors >> mainMenu
     "help" -> printHelp >> mainMenu
     "exit" -> putStrLn "Thanks for using TVRecommender!"
@@ -24,6 +25,7 @@ mainMenu = do
 printHelp :: IO () --Gibt Liste der möglichen Befehle sowie Aufforderung zur Eingabe aus
 printHelp = do
   putStrLn "\nThis Program supports the following commands:"
+  putStrLn "\t 'add actor' ... add a given name to your list of favourite actors"
   putStrLn "\t 'list actors' ... shows a list of all your favourite actors"
   putStrLn "\t 'help' ... shows this message"
   putStrLn "\t 'exit' ... terminate the application"
@@ -38,3 +40,13 @@ getActors = do
     putStrLn actorText
     hClose handle
   else putStrLn "\nFile 'actors.txt' does not exist yet. Create it by adding your first actor."
+
+addActor :: String -> IO ()
+addActor name = do
+  fileExists <- doesFileExist "actors.txt"
+  if fileExists then do
+    actors <- readFile "actors.txt"
+    let actorList = lines actors
+    if name `notElem` actorList then appendFile "actors.txt" ("\n" ++ name)
+    else putStrLn ""
+  else writeFile "actors.txt" "" 
