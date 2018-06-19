11ba090dd4dfaafafc3a23373b42dce6ec677b76
xK3v <kazianschuetz.kevin@gmail.com>
2018-06-19 17:25:07 +0200

    added comments, cleanup, renames

--- TVRecommender.hs
+++ TVRecommender.hs
@@ -8,6 +8,8 @@ import Data.Char
 import qualified Data.ByteString.Lazy.Char8 as L8
 import Text.Printf (printf)
 import System.Directory
+import qualified Data.Vector as V
+
 --import Control.Monad
 --import Control.Monad.IO.Class
 
@@ -24,7 +26,7 @@ main = do
   setLocaleEncoding GHC.IO.Encoding.utf8
   putStrLn ""
   putStrLn "Loading TVRecommender..."
-  let !info = parseSite
+  let !info = parseSite --Downloads the information about all the broadcasts
   putStrLn "Got Content!"
   printHelp
   mainMenu info
@@ -58,24 +60,32 @@ printHelp = do
 
 parseSite :: IO [(Int,String,String,String,String,String,[String])]
 parseSite = do
-  site           <- simpleHttp "https://www.tele.at/tv-programm/2015-im-tv.html?stationType=-1&start=0&limit=500&format=raw"
-  let parsed     = readString [withParseHTML yes, withWarnings no] $ L8.unpack site
-  sender         <- runX $ parsed //> hasAttrValue "class" (== "station") //> removeAllWhiteSpace //> deep getText
-  zeiten         <- runX $ parsed //> hasAttrValue "class" (isInfixOf "broadcast") //> hasName "strong" >>> deep getText
-  sendungen_ws   <- runX $ parsed //> hasAttrValue "class" (=="title") //> removeAllWhiteSpace //> getText
-  genre_ws       <- runX $ parsed //> hasAttrValue "class" (=="genre") >>> removeAllWhiteSpace >>> deep getText
-  link_short     <- runX $ parsed //> hasAttrValue "class" (== "title") //> hasName "a" >>> getAttrValue "href"
-  -- TODO: nur erste sendung jedes "bc-item" nehmen
+  --downloading website:
+  siteString           <- simpleHttp "https://www.tele.at/tv-programm/2015-im-tv.html?stationType=-1&start=0&limit=500&format=raw"
+  let site     = readString [withParseHTML yes, withWarnings no] $ L8.unpack siteString
+
+  --filtering the relevant information:
+  sender         <- runX $ site //> hasAttrValue "class" (== "station") //> removeAllWhiteSpace //> deep getText
+  zeiten         <- runX $ site //> hasAttrValue "class" (isInfixOf "broadcast") //> hasName "strong" >>> deep getText
+  sendungen_ws   <- runX $ site //> hasAttrValue "class" (=="title") //> removeAllWhiteSpace //> getText
+  genre_ws       <- runX $ site //> hasAttrValue "class" (=="genre") >>> removeAllWhiteSpace >>> deep getText
+  link_short     <- runX $ site //> hasAttrValue "class" (== "title") //> hasName "a" >>> getAttrValue "href"
+
+  --doing some work on the information:
   let sendungen = map (filter (/= '\n') . filter (/= '\t')) sendungen_ws
   let genre     = map (filter (/= '\n') . filter (/= '\t')) genre_ws
   let link      = map (\str -> "https://www.tele.at" ++ str) link_short
+
+  --creating tuple with basic information
   let zipped    = zip5 zeiten sender sendungen genre link
-  --let zipped = zip6 [1..length sendungen + 1] zeiten sender sendungen genre link
+
+  --sorting by station name
   let sorted    = sortOn (\(_,send,_,_,_) -> map toLower send) zipped
+
+  --adding numbering
   --let numbered = map unFoldTuple $ zip [1..length sendungen + 1] sorted
-  --let numbered = zipWith (curry unFoldTuple) [1..length sendungen + 1] sorted
-  let numbered = zipWith (curry (\(n,(a,b,c,d,e)) -> (n,a,b,c,d,e))) [1..length sendungen + 1] sorted
-  mapM parseDetails numbered
+  let numbered  = zipWith (curry (\(n,(a,b,c,d,e)) -> (n,a,b,c,d,e))) [1..length sendungen + 1] sorted
+  mapM parseDetails numbered --Map der parseDetails function und Return der IO [IO ()]
 
 
 {-
@@ -84,28 +94,36 @@ unFoldTuple (n,(a,b,c,d,e)) = (n,a,b,c,d,e)
 -}
 
 
-parseDetails :: (Int,String,String,String,String,String) -> IO (Int,String,String,String,String,String,[String])
+parseDetails :: (Int,String,String,String,String,String) -> IO (Int,String,String,String,String,String,[String]) --adding text and actors
 parseDetails (n,a,b,c,d,link) = do
-  detailSite <- simpleHttp link
-  let detailsParsed = readString [withParseHTML yes, withWarnings no] $ L8.unpack detailSite
-  text <- runX $ detailsParsed //> hasAttrValue "class" (== "long-text") >>> deep getText
-  actors <- runX $ detailsParsed //> hasAttrValue "class" (== "actor") //> hasName "span" >>> deep getText
-  --let detailBcs = (n,a,b,c,d,head text,actors)
-  let detailBcs = (n,a,b,c,d,if null text then "No information available" else head text,actors)
+  --downloading detailed website
+  detailSiteString <- simpleHttp link
+  let detailSite = readString [withParseHTML yes, withWarnings no] $ L8.unpack detailSiteString
+
+  --getting relevant information (text and actors)
+  text   <- runX $ detailSite //> hasAttrValue "class" (== "long-text") >>> deep getText
+  actors <- runX $ detailSite //> hasAttrValue "class" (== "actor") //> hasName "span" >>> deep getText
+
+  --putting together new tuple and dealing with missing information
+  let detailBcs = (n,a,b,c,d,if null text then "No information available" else head text,if null actors then ["-"] else actors)
   return detailBcs
 
 
-listBroadcasts :: IO [(Int,String,String,String,String,String,[String])] -> IO ()
+listBroadcasts :: IO [(Int,String,String,String,String,String,[String])] -> IO () --prints overview of todays program
 listBroadcasts info = do
-  let addTuple (n,t_zeiten,t_sender,t_sendungen,t_genre,_,_) = printf "%03d." n ++ "\t" ++ t_zeiten ++ "\t" ++ printf "%- 16s" t_sender ++ "\t" ++ t_sendungen ++ ", " ++ t_genre
   broadcasts <- info
+
+  --adding tuples to one continuous string
+  let addTuple (n,t_zeiten,t_sender,t_sendungen,t_genre,_,_) = printf "%03d." n ++ "\t" ++ t_zeiten ++ "\t" ++ printf "%- 16s" t_sender ++ "\t" ++ t_sendungen ++ ", " ++ t_genre
+  --printing list command
   putStrLn $ unlines $ map addTuple broadcasts
   --mapM_ putStrLn $ map addTuple zipped
 
 
-showBroadcast :: Int -> IO [(Int,String,String,String,String,String,[String])] -> IO ()
+showBroadcast :: Int -> IO [(Int,String,String,String,String,String,[String])] -> IO () --prints detailed information of a specific broadcast
 showBroadcast n info = do
   bclist <- info
+
   let bcinfo = bclist !! (n-1)
   let addDetails (_,t_zeit,t_sender,t_sendung,t_genre,t_text,t_actors) = "\nTitle: " ++ t_sendung ++ " (" ++ t_genre ++ ") \n" ++ t_zeit ++ " " ++ t_sender ++ "\n\n" ++ t_text ++ "\n\nActors:\n" ++ unlines t_actors
   putStrLn $ addDetails bcinfo
